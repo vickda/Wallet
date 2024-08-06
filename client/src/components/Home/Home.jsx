cbfaa606b87d61ac07cb7134dcb0935c0b664330
vickda <vdas@hawk.iit.edu>
2024-08-05 23:17:55 -0400

    Navbar & Signout Added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,28 @@
+import NavBar from "../Navbar/Navbar";
+import "./Home.css";
+
 const Home = ({ userName }) => {
-  console.log(userName);
+  const links = [
+    {
+      id: 1,
+      name: "People",
+      location: "/",
+    },
+    {
+      id: 2,
+      name: "Groups",
+      location: "/group",
+    },
+    {
+      id: 3,
+      name: "User Profile",
+      location: "/profile",
+    },
+  ];
 
   return (
     <>
+      <NavBar links={links} />
       <h1>Welcome {userName || "User"} To The Dashboard</h1>
     </>
   );
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { Link, Navigate } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/logo.jpg";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebase/firebaseConfig";
+
+const Navbar = ({ links }) => {
+  const [open, setOpen] = useState(false);
+  console.log(links);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    try {
+      signOut(auth);
+      Navigate({ to: "/signin" });
+    } catch (error) {
+      console.log("Unable to Signout", error.message);
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-logo">
+        <img className="nav-logo" src={logo} alt="Logo" />
+      </Link>
+      <div onClick={handleClick} className="nav-icon">
+        {open ? <FiX /> : <FiMenu />}
+      </div>
+      <ul className={open ? "nav-links nav-active" : "nav-links"}>
+        {links.map(({ id, name, location }) => {
+          return (
+            <li key={id} className="nav-item">
+              <Link to={location} className="nav-link" onClick={closeMenu}>
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+
+        <li className="nav-item nav-link" onClick={() => handleLogout()}>
+          Signout
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
